63270560ad66ebff108f4b050789598b4b28f0ff
janak <janakrajbhatta@gmail.com>
2016-04-15 16:46:49 +0545

    key generate

--- config/app.php
+++ config/app.php
@@ -78,7 +78,7 @@ return [
     |
      */
 
-    'key'             => env('APP_KEY', 'SomeRandomStringssssssssssss'),
+    'key'             => env('APP_KEY', 'ITlYGSRDkzNUVXhooByd1SHkT9ehdQka'),
 
     'cipher'          => 'AES-256-CBC',
 
